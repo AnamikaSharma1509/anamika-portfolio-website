3508b896b1ecba0656857543426a741efdf05f24
Anamika Sharma <anamikasharmamgahv@gmail.com>
2025-09-22 13:23:34 +0530

    added some tailwind changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    "./index.html",                // root HTML file
+    "./src//*.{js,jsx,ts,tsx}",  // scan everything in src, including components & pages
+    "./src/components//*.{js,jsx,ts,tsx}", // explicit components folder
+    "./src/pages//*.{js,jsx,ts,tsx}",      // explicit pages folder
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+      },
+    },
   },
-  plugins: [],
-}
-
+  plugins: [require("tailwindcss-animate")],
+};
